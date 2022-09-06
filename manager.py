5ce8754362295cddb48506286045730a56050e90
pvernier <pvernier82@gmail.com>
2022-09-06 16:31:01 +0200

    Initial commit

---
+++ manager.py
@@ -0,0 +1,183 @@
+import requests
+import pandas as pd
+import numpy as np
+
+
+class Manager:
+    def __init__(self, url_api, format, token) -> None:
+        self.url_api = url_api
+        self.format = format
+        self.token = token
+        self.headers = self._build_headers(self.token)
+        self._params = {"format": self.format}
+
+    def _build_headers(self, token: str) -> str:
+        return {"Authorization": f'Token {token}', "Accept": f"application/{self.format}"}
+
+    def get_forms(self) -> dict:
+        '''Return a dictionary of all the forms the user has access to with its token.
+        The keys are the UID and the values, the name of the forms'''
+        url_assets = f'{self.url_api}/assets.json'
+        res = requests.get(
+            url=url_assets, headers=self.headers, params=self._params)
+
+        # If error while fetching the data, return an empty dict
+        if res.status_code != 200:
+            return {}
+
+        forms = {}
+        for form in res.json()['results']:
+            forms[form['uid']] = form['name']
+        return forms
+
+    def get_url_data_form(self, uid: str) -> str:
+        '''Given the uid of a form
+        returns the URL of the data in JSON'''
+
+        return f'{self.url_api}/assets/{uid}/data?format={self.format}'
+
+    def get_url_data_metadata(self, uid: str) -> str:
+        '''Given the uid of a form
+        returns the URL of the metadata in JSON'''
+
+        return f'{self.url_api}/assets/{uid}/?format={self.format}'
+
+    def fetch_form_data(self, uid: str, with_labels: bool = False) -> pd.DataFrame:
+        '''Given the uid of a form
+        returns the data as a Pandas DataFrame
+        The boolean `with_labels` indicates if we return the DataFrame with labels
+        for the columns name and values or without (default)'''
+
+        # Get the URL of the data for that form
+        url = self.get_url_data_form(uid)
+
+        # Fetch the data
+        res = requests.get(url=url, headers=self.headers, params=self._params)
+
+        # If error while fetching the data, return an empty DF
+        if res.status_code != 200:
+            return pd.DataFrame()
+
+        data = res.json()['results']
+        df = pd.DataFrame(data)
+
+        if with_labels:
+            self._fetch_form_labels(df, uid)
+
+        return df
+
+    def _fetch_form_labels(self, df: pd.DataFrame, uid: str) -> None:
+        '''TO DO'''
+        # Fetch the metadata
+        url_meta = self.get_url_data_metadata(uid)
+        res_meta = requests.get(
+            url=url_meta, headers=self.headers, params=self._params)
+
+        fields = res_meta.json()['content']['survey']
+        form_columns = []
+
+        # Columns labels
+        for field in fields:
+            try:
+                name = field['name']
+            except KeyError:
+                name = field['$autoname']
+
+            # The JSON object doesn't have properties for empty column
+            # so we need to add them here so we don't get issues later
+            if name not in df.columns:
+                df[name] = np.nan
+
+            if 'label' in field:
+                label = field['label'][0]
+            else:
+                label = field['name']
+
+            if field['type'] == 'select_one':
+                select_one = field['select_from_list_name']
+            else:
+                select_one = None
+
+            if field['type'] == 'select_multiple':
+                select_multiple = field['select_from_list_name']
+            else:
+                select_multiple = None
+
+            form_columns.append({'name': name, 'label': label, 'select_one': select_one,
+                                'select_multiple': select_multiple})
+            dict_rename = {}
+            for column in form_columns:
+                dict_rename[column['name']] = column['label']
+
+        df.rename(columns=dict_rename, inplace=True)
+
+        # Values labels
+        formatted_choices = {}
+        choices = res_meta.json()['content']['choices']
+        for choice in choices:
+            if choice['list_name'] not in formatted_choices:
+                formatted_choices[choice['list_name']] = []
+            formatted_choices[choice['list_name']].append(
+                {'name': choice['name'], 'label': choice['label'][0]})
+
+        for column in form_columns:
+            if column['select_one'] or column['select_multiple']:
+                if column['select_one']:
+                    uid_choice = column['select_one']
+
+                    for choice in formatted_choices[uid_choice]:
+                        df.loc[df[column['label']] == choice['name'],
+                               column['label']] = choice['label']
+
+                else:
+                    uid_choice = column['select_multiple']
+                column['value_labels'] = formatted_choices[uid_choice]
+
+        # For multiple values we have to loop again
+        for column in form_columns:
+            if column['select_multiple']:
+                unique_values = list(df[column['label']].unique())
+
+                for unique in unique_values:
+                    if pd.isna(unique):
+                        pass
+                    else:
+                        combinations = unique.split()
+                        label = [c['label']
+                                 for c in column['value_labels'] if c['name'] in combinations]
+                        label_formatted = ', '.join(label)
+
+                        df.loc[df[column['label']] == unique,
+                               column['label']] = label_formatted
+
+    def remove_metadata(self, df: pd.DataFrame) -> pd.DataFrame:
+        '''Remove the metadata columns (meaning the ones starting with an '_',
+        plus the 4 columns: 'formhub/uuid', 'meta/instanceID', 'start', 'end') 
+        of the DataFrame'''
+
+        metadata_columns = [
+            column for column in df.columns if column.startswith('_')]
+
+        others = ['formhub/uuid', 'meta/instanceID', 'start',
+                  'end', 'today',  'username', 'deviceid']
+
+        # We only keep the columns that are in the DataFrame
+        others = [
+            o for o in others if o in df.columns]
+
+        metadata_columns = metadata_columns + others
+
+        df.drop(metadata_columns, axis=1, inplace=True)
+
+    def split_gps_coords(self, df: pd.DataFrame, gps_field: str) -> pd.DataFrame:
+
+        if gps_field not in df.columns:
+            raise ValueError(
+                f"The DataFrame doesn't contain the column '{gps_field}'")
+
+        df[['latitude', 'longitude', 'altitude', 'gps_precision']
+           ] = df[gps_field].str.split(' ', expand=True)
+
+        df.drop(gps_field, axis=1, inplace=True)
+
+        return df
---
+++ utility.py
@@ -0,0 +1,109 @@
+import pandas as pd
+
+
+def clean_df(df: pd.DataFrame, columns: list) -> pd.DataFrame:
+    '''
+    Given a Pandas DataFrame and a list of columns
+    only keep the columns of the DataFrame that are in the list
+    '''
+
+    return df[columns]
+
+
+def lowercase_columns_name(df: pd.DataFrame) -> None:
+    '''
+    Lowercase the name of all columns of the DataFrame 'df'
+    '''
+    df.columns = df.columns.str.lower()
+
+
+def lowercase_column_values(df: pd.DataFrame, column: str) -> None:
+    '''
+    Lowercase the values of the column 'column' in the DataFramce 'df'
+    '''
+    df[column] = df[column].str.lower()
+
+
+def capitalize_column_values(df: pd.DataFrame, column: str) -> None:
+    '''
+    Capitalize the values of the column 'column' in the DataFramce 'df'
+    '''
+    df[column] = df[column].str.capitalize()
+
+
+def trim_columns_values(df: pd.DataFrame) -> pd.DataFrame:
+    '''
+    Trim whitespace from ends of each value across all series in dataframe
+    Source: https://stackoverflow.com/questions/40950310/strip-trim-all-strings-of-a-dataframe
+    '''
+    def trim_ws(x): return x.strip() if isinstance(x, str) else x
+    def trim_pt(x): return x.strip('.') if isinstance(x, str) else x
+    df_temp = df.applymap(trim_ws)
+    return df_temp.applymap(trim_pt)
+
+
+def reconcile_columns(df: pd.DataFrame, col1: str, col2: str, new_column: str, criteria=float("nan")) -> None:
+    '''
+    Given a DF, reconcile its 2 columns `col1` and `col2` into a new column `new_column`, as follow:
+        if `col1` is null get the values from `col2` into `new_column`
+        if `col1` is not null get the values from `col1` into `new_column`
+    This means that `col1` is considered more important than `col2`
+    Then delete columns `col1` and `col2`
+    '''
+
+    # `_temp` is a temporary name for `new_column`
+
+    if pd.isna(criteria):
+        df.loc[(df[col1].isnull()), '_temp'] = df[col2]
+
+        df.loc[(df[col1].notnull()), '_temp'] = df[col1]
+    elif type(criteria) == str:
+        df.loc[(df[col1] == criteria), '_temp'] = df[col2]
+
+        df.loc[(df[col1] != criteria), '_temp'] = df[col1]
+    else:
+        raise TypeError(
+            f"The argument `criteria` is of type: `{type(criteria)}`. It should be of type `<class 'str'>` or equal to `NaN`")
+
+    # Delete the 2 columns
+    df.drop(columns=[col1, col2], inplace=True)
+
+    # Rename column `_temp` to `new_column`
+    df.rename(columns={"_temp": new_column}, inplace=True)
+
+
+def reconcile_columns_append(df: pd.DataFrame, col1: str, col2: str, new_column: str, to_replace: str) -> None:
+    '''TODO
+    XXX: See if can merge this function and the one above into 1
+    '''
+
+    # If the column is not of type str we convert it
+    # This can happen for columns that are empty and were
+    # not in the JSON object so we created empty column containing NaN
+    if df[col1].dtype != "object":
+        df[col1] = df[col1].astype(str)
+
+    selection = df[col1].str.contains(to_replace, na=False)
+    for index, row in df.loc[selection].iterrows():
+        df.loc[index, col1] = row[col1].replace(to_replace, row[col2])
+
+    df['_temp'] = df[col1]
+
+    # Delete the 2 columns
+    df.drop(columns=[col1, col2], inplace=True)
+
+    # Rename column `_temp` to `new_column`
+    df.rename(columns={"_temp": new_column}, inplace=True)
+
+
+def fix_typos(df: pd.DataFrame, column: str, typos: list) -> None:
+    for typo in typos:
+        df.loc[df[column] == typo[0], column] = typo[1]
+
+
+def fillna(df: pd.DataFrame, column: str) -> None:
+    df[column] = df[column].fillna(0)
+
+
+def convert_toint(df: pd.DataFrame, column) -> None:
+    df[column] = df[column].astype(int)
